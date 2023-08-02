e1654d674d350826f93cea0a46000c8a4ba8041c
RaulPabianCruz <ito.pabian@gmail.com>
2023-08-01 17:36:21 -0700

    Add the Operate function
    Operate function is added which is calls the four basic math operation
    functions.

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function divide(firstNum, secondNum){
     return firstNum / secondNum;
 }
 
+function operate(firstOperand, operator, secondOperand){
+    switch(operator){
+        case "+":
+            return add(firstOperand, secondOperand);
+        case "-":
+            return subtract(firstOperand, secondOperand);
+        case "*":
+            return multiply(firstOperand, secondOperand);
+        case "/": 
+            return divide(firstOperand, secondOperand); 
+    }
+}
+
